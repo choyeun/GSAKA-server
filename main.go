285d68cb2bc367a6cbfec6a7d14ec4627f7d9549
choyeun <choyeun0372@gmail.com>
2023-04-07 07:29:17 +0900

    .

--- main.go
+++ main.go
@@ -1,47 +1,19 @@
 package main
 
-import "fmt"
-import "math/rand"
-
-
-// from https://dejavuqa.tistory.com/360
-func quickSort(arr []int) []int {
-
-	if len(arr) <= 1 {
-		return arr
-	}
-
-	median := arr[rand.Intn(len(arr))]
-
-	lowPart := make([]int, 0, len(arr))
-	highPart := make([]int, 0, len(arr))
-	middlePart := make([]int, 0, len(arr))
-
-	for _, item := range arr {
-		switch {
-		case item < median:
-			lowPart = append(lowPart, item)
-		case item == median:
-			middlePart = append(middlePart, item)
-		case item > median:
-			highPart = append(highPart, item)
-		}
-	}
-
-	lowPart = quickSort(lowPart)
-	highPart = quickSort(highPart)
-
-	lowPart = append(lowPart, middlePart...)
-	lowPart = append(lowPart, highPart...)
-
-	return lowPart
-}
-
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, world!")
+	})
 
-	fmt.Println("hello world")
-
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 /**
