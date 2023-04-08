ecfb1d6d12c43f1919d0d7dc4d68915b654852d1
choyeun <choyeun0372@gmail.com>
2023-04-08 12:00:17 +0000

    .

--- main.go
+++ main.go
@@ -3,20 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, world!")
+		fmt.Fprintf(w, "Hello, world! Nice to meet you")
 	})
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
 
 /**
+git pull
 go run main.go
 git add .
 git commit -m "."
